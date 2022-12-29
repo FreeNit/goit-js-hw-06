9f095a2b81e830f52a4d14ea1c76202b716a8a06
Dimon <bolilui.dmytro@gmail.com>
2022-12-29 12:05:32 +0200

    Task9 - Done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// * Get main elements
+const changeColorBtnEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+changeColorBtnEl.addEventListener('click', () => {
+  spanEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+});
